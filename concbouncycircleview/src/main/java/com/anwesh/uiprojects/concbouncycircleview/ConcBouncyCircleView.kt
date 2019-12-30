a49eea9fdc639402e7f693b89a6928361614b616
Anwesh43 <anweshthecool0@gmail.com>
2019-12-30 10:38:47 +0530

    adding CBCNode class

--- concbouncycircleview/src/main/java/com/anwesh/uiprojects/concbouncycircleview/ConcBouncyCircleView.kt
+++ concbouncycircleview/src/main/java/com/anwesh/uiprojects/concbouncycircleview/ConcBouncyCircleView.kt
@@ -118,4 +118,45 @@ class ConcBouncyCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CBCNode(var i : Int, val state : State = State()) {
+
+        private var next : CBCNode? = null
+        private var prev : CBCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CBCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCBCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CBCNode {
+            var curr : CBCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
