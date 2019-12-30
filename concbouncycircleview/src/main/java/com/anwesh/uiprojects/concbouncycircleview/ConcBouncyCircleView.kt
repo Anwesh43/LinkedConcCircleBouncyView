403346149ced6c839900cbf702ec4794a2ff4cf8
Anwesh43 <anweshthecool0@gmail.com>
2019-12-30 10:33:34 +0530

    adding ConcBouncyCircleView class

--- concbouncycircleview/src/main/java/com/anwesh/uiprojects/concbouncycircleview/ConcBouncyCircleView.kt
+++ concbouncycircleview/src/main/java/com/anwesh/uiprojects/concbouncycircleview/ConcBouncyCircleView.kt
@@ -53,3 +53,21 @@ fun Canvas.drawCBCNode(i : Int, scale : Float, paint : Paint) {
     drawConcBouncyCircles(scale, size, paint)
     restore()
 }
+
+class ConcBouncyCircleView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
