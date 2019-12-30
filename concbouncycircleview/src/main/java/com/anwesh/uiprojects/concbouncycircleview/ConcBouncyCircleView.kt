00c28343cb807e5135526cb79dbe6ebaad42cc84
Anwesh43 <anweshthecool0@gmail.com>
2019-12-30 10:32:08 +0530

    adding Canvas extension methods

--- concbouncycircleview/src/main/java/com/anwesh/uiprojects/concbouncycircleview/ConcBouncyCircleView.kt
+++ concbouncycircleview/src/main/java/com/anwesh/uiprojects/concbouncycircleview/ConcBouncyCircleView.kt
@@ -26,3 +26,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawConcBouncyCircle(i : Int, scale : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.sinify().divideScale(i, circles)
+    val r : Float = (size / (circles + 2)) * (i + 2)
+    drawArc(RectF(-r, -r, r, r), 0f, 360f * sf, false, paint)
+}
+
+fun Canvas.drawConcBouncyCircles(scale : Float, size : Float, paint : Paint) {
+    for (j in 0..(circles - 1)) {
+        drawConcBouncyCircle(j, scale, size, paint)
+    }
+}
+
+fun Canvas.drawCBCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    save()
+    translate((i + 1) * gap, h / 2)
+    drawConcBouncyCircles(scale, size, paint)
+    restore()
+}
