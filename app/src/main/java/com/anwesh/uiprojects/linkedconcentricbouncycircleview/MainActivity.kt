74ee7fd49f32235764fbec9965a3dc543b3f9694
Anwesh43 <anweshthecool0@gmail.com>
2019-12-30 10:46:12 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedconcentricbouncycircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedconcentricbouncycircleview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedconcentricbouncycircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.concbouncycircleview.ConcBouncyCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ConcBouncyCircleView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
